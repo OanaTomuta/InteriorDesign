e372c2649bd20a6334a16da67ae1ff7ed6b7d90b
Oana Tomuta <tomutaoana@gmail.com>
2020-10-06 00:00:42 +0300

    be: initial commit

---
+++ be/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const port = 4000;
+
+app.get('/api', (req, res) => {
+    res.json('Hello');
+
+});
+
+app.listen(port, () => {
+    console.log(`Oana's server running on port ${port}`);
+})
